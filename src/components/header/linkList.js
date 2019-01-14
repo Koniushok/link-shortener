9926bad47fd29a2afa7b2506a9613fc8c5b3f79d
Valera <Koniushok@users.noreply.github.com>
2019-01-14 07:15:48 -0900

    fix navBar

---
+++ src/components/header/linkList.js
@@ -0,0 +1,30 @@
+export const guestLinks = [
+  {
+    id: 0,
+    url: "/",
+    title: "Home"
+  },
+  {
+    id: 1,
+    url: "/login/1",
+    title: "Sing In"
+  },
+  {
+    id: 2,
+    url: "/signup",
+    title: "Sign Up"
+  }
+];
+
+export const userLinks = [
+  {
+    id: 0,
+    url: "/",
+    title: "Home"
+  },
+  {
+    id: 1,
+    url: "/logout",
+    title: "Logout"
+  }
+];
--- src/components/header/navBar.jsx
+++ src/components/header/navBar.jsx
@@ -1,49 +1,14 @@
 // @flow
 import React from "react";
 import { Link } from "react-router-dom";
-
-const guestLinks = [
-  {
-    id: 0,
-    url: "/",
-    title: "Home"
-  },
-  {
-    id: 1,
-    url: "/authorization",
-    title: "Sing In"
-  },
-  {
-    id: 2,
-    url: "/registration",
-    title: "Sign Up"
-  }
-];
-
-const userLinks = [
-  {
-    id: 0,
-    url: "/",
-    title: "Home"
-  },
-  {
-    id: 1,
-    url: "",
-    title: ""
-  },
-  {
-    id: 2,
-    url: "/logout",
-    title: "Logout"
-  }
-];
-
-const Links = userLinks;
+import { guestLinks as links } from "./linkList";
 
 const NavBar = () => (
   <div>
-    {Links.map(link => (
-      <Link to={link.url} key={link.id}>{link.title}</Link>
+    {links.map(link => (
+      <Link to={link.url} key={link.id}>
+        {link.title}
+      </Link>
     ))}
   </div>
 );
