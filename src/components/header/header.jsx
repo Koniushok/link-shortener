ef87b9eaa19c7cf3138a8391687e53d5ff2848ee
Valera <Koniushok@users.noreply.github.com>
2019-01-15 06:30:03 -0900

    fix navbar

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,21 @@
 // @flow
-import React from "react";
-import NavBar from "./navBar";
-import { HeaderDiv } from "../styled-components/divs";
+import React, { Fragment } from "react";
+import { HeaderDiv } from "../styled-components/wrappers";
+import NavLink from "../styled-components/navLink";
+
+const auth = false;
 
 const Header = () => (
   <HeaderDiv>
-    <NavBar />
+    <NavLink to="/" exact>Home</NavLink>
+    {auth ? (
+      <Fragment>
+        <NavLink to="/signup">Sign Up</NavLink>
+        <NavLink to="/login">Sing In</NavLink>
+      </Fragment>
+    ) : (
+      <NavLink to="/logout">Logout</NavLink>
+    )}
   </HeaderDiv>
 );
 
