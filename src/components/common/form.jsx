ff55c756a00b1c5d224ff14d337757028e46754e
Valera <Koniushok@users.noreply.github.com>
2019-01-16 05:04:44 -0900

    flow

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,15 +1,23 @@
+// @flow
 import React, { Component, Fragment } from "react";
 import Joi from "joi-browser";
 import { Button } from "../styled-components/buttons";
 import { Input, Lable, LableError } from "../styled-components/formComponenta";
 
-class Form extends Component {
-  state = {
-    data: {},
-    errors: {},
-    result: null
-  };
-
+type State = {
+  data: {},
+  errors: {},
+  result: string
+};
+type inputObject = {
+  id: string,
+  label: string,
+  name: string,
+  type: string
+};
+class Form<T1, T2> extends Component<T1, T2 & State> {
+  schema: {};
+  doSubmit: () => void;
   validate = () => {
     const options = { abortEarly: false };
     const { error } = Joi.validate(this.state.data, this.schema, options);
@@ -19,44 +27,45 @@ class Form extends Component {
     return errors;
   };
 
-  handleSubmit = e => {
+  handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
     e.preventDefault();
     const errors = this.validate();
-    this.setState({ errors: errors || {}, result: null });
+    this.setState({ errors: errors || {}, result: "" });
     if (errors) return;
     this.doSubmit();
   };
 
-  handleChange = ({ currentTarget: input }) => {
+  handleChange = (e: SyntheticInputEvent<HTMLInputElement>) => {
+    const { currentTarget: input } = e;
     const data = { ...this.state.data };
     data[input.name] = input.value;
     this.setState({ data });
   };
 
-  renderButton(label) {
+  renderButton(label: string) {
     return <Button>{label}</Button>;
   }
 
-  renderAlert(error, message) {
-    if (error) return <p>{props.error}</p>;
-    if (message) return <p>{props.message}</p>;
+  renderAlert(error: string, message: string) {
+    if (error) return <p>{error}</p>;
+    if (message) return <p>{message}</p>;
 
     return null;
   }
 
-  renderInput(inputObj) {
+  renderInput({ id, label, name, type }: inputObject) {
     return (
       <Fragment>
-        <Lable htmlFor={inputObj.id}>{inputObj.label}</Lable>
+        <Lable htmlFor={id}>{label}</Lable>
         <Input
-          name={inputObj.name}
-          type={inputObj.type}
-          id={inputObj.id}
+          name={name}
+          type={type}
+          id={id}
           onChange={this.handleChange}
-          value={this.state.data[inputObj.name]}
+          value={this.state.data[name]}
         />
-        {this.state.errors[inputObj.name] && (
-          <LableError>{this.state.errors[inputObj.name]}</LableError>
+        {this.state.errors[name] && (
+          <LableError>{this.state.errors[name]}</LableError>
         )}
       </Fragment>
     );
