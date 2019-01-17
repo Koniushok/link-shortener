f480a644934a47f04c9cac85af1a8812a7052409
Valera <Koniushok@users.noreply.github.com>
2019-01-17 12:59:31 -0900

    add render tags

--- src/containers/linkCreator.jsx
+++ src/containers/linkCreator.jsx
@@ -1,6 +1,7 @@
 // @flow
 import React, { Component } from "react";
 import styled from "styled-components";
+import _ from "lodash";
 import { XCircle } from "styled-icons/boxicons-regular/XCircle";
 import InputLabel from "../components/inputLabel";
 import Button from "../components/buttons";
@@ -24,7 +25,7 @@ const TagWrapper = styled.div`
   align-items: center;
   background: #d6d6d6ab;
 `;
-const TagButton = styled(XCircle)`
+const ButtonDelete = styled(XCircle)`
   margin-left: 8px;
   border-radius: 50%;
   display: inline-flex;
@@ -35,10 +36,10 @@ const TagButton = styled(XCircle)`
   height: 20px;
   cursor: pointer;
 `;
-const Tag = ({ text }: { text: string }) => (
+const Tag = ({ text, handleDelete }: { text: string }) => (
   <TagWrapper>
     <span>{text}</span>
-    <TagButton />
+    <ButtonDelete onClick={handleDelete} />
   </TagWrapper>
 );
 
@@ -46,12 +47,13 @@ type State = {
   data: {
     link: string,
     description: string,
-    tags: string
+    tag: string,
+    tags: Array<string>
   },
   error: {
     link: string,
     description: string,
-    tags: string
+    tag: string
   }
 };
 class LinkCreator extends Component<{}, State> {
@@ -59,21 +61,37 @@ class LinkCreator extends Component<{}, State> {
     data: {
       link: "link",
       description: "description",
-      tags: "tags"
+      tag: "tags",
+      tags: []
     },
     error: {
       link: "Error",
       description: "",
-      tags: "Error"
+      tag: "Error"
     }
   };
-
+  addTag = data => {
+    if (_.endsWith(_.trimStart(data.tag), " ")) {
+      const tag = _.camelCase(data.tag);
+      if (_.indexOf(data.tags, tag) === -1) data.tags.push(tag);
+      data.tag = "";
+    }
+    return data;
+  };
+  deleteTag = tag => {
+    const data = { ...this.state.data };
+    _.remove(data.tags, function(n) {
+      return n === tag;
+    });
+    this.setState({ data });
+  };
   handleChange = (e: SyntheticInputEvent<HTMLInputElement>) => {
     const { currentTarget: input } = e;
     this.setState(prevState => {
       const { data } = prevState;
       data[input.name] = input.value;
-      return { data };
+      if (input.name === "tag") return this.addTag(data);
+      return data;
     });
   };
 
@@ -90,17 +108,21 @@ class LinkCreator extends Component<{}, State> {
           onChange={this.handleChange}
         />
         <InputLabel
-          id="tagsID"
-          label="Tags"
-          name="tags"
-          error={error.tags}
-          value={data.tags}
+          id="tagID"
+          label="Tag"
+          name="tag"
+          error={error.tag}
+          value={data.tag}
           onChange={this.handleChange}
         />
         <div>
-          <Tag text="tag1" />
-          <Tag text="tag1" />
-          <Tag text="tag1" />
+          {this.state.data.tags.map(tag => (
+            <Tag
+              text={tag}
+              key={tag}
+              handleDelete={() => this.deleteTag(tag)}
+            />
+          ))}
         </div>
         <InputLabel
           id="descriptionID"
