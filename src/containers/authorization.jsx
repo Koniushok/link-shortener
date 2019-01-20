8d062f226836a32af978181664d3dd5ca13d6796
Valera <Koniushok@users.noreply.github.com>
2019-01-20 05:37:56 -0900

    add functionality to authorization components

--- src/containers/authorization.jsx
+++ src/containers/authorization.jsx
@@ -1,6 +1,71 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
+import InputLabel from "../components/inputLabel";
+import Button from "../components/buttons";
 
-const Authorization = () => <p>Authorization</p>;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  & > * {
+    margin-bottom: 20px;
+  }
+`;
+type State = {
+  loginData: {
+    login: string,
+    password: string
+  },
+  error: {
+    login: string,
+    password: string
+  }
+};
+class Authorization extends Component<any, State> {
+  state = {
+    loginData: {
+      login: "1",
+      password: "2"
+    },
+    error: {
+      login: "",
+      password: ""
+    }
+  };
+
+  handleChange = (e: SyntheticInputEvent<HTMLInputElement>) => {
+    const { currentTarget: input } = e;
+    this.setState(prevState => {
+      const { loginData } = prevState;
+      loginData[input.name] = input.value;
+      return { loginData };
+    });
+  };
+
+  render() {
+    const { loginData, error } = this.state;
+    return (
+      <Form autoComplete="off">
+        <InputLabel
+          label="Login"
+          error={error.login}
+          name="login"
+          value={loginData.login}
+          onChange={this.handleChange}
+        />
+        <InputLabel
+          type="password"
+          label="Password"
+          name="password"
+          error={error.password}
+          value={loginData.password}
+          onChange={this.handleChange}
+        />
+        <Button left>Log in</Button>
+      </Form>
+    );
+  }
+}
 
 export default Authorization;
