550db3bf81dfa41dd573810aeb3c7eaad41d06d6
Valera <Koniushok@users.noreply.github.com>
2019-01-20 05:37:38 -0900

    add functionality to registration components

--- src/containers/registration.jsx
+++ src/containers/registration.jsx
@@ -1,6 +1,94 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
+import InputLabel from "../components/inputLabel";
+import Button from "../components/buttons";
 
-const Registration = () => <p>Registration</p>;
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  & > * {
+    margin-bottom: 20px;
+  }
+`;
+type State = {
+  userData: {
+    login: string,
+    name: string,
+    surname: string,
+    password: string
+  },
+  error: {
+    login: string,
+    name: string,
+    surname: string,
+    password: string
+  }
+};
+
+class Registration extends Component<any, State> {
+  state = {
+    userData: {
+      login: "",
+      name: "",
+      surname: "",
+      password: ""
+    },
+    error: {
+      login: "",
+      name: "",
+      surname: "",
+      password: ""
+    }
+  };
+
+  handleChange = (e: SyntheticInputEvent<HTMLInputElement>) => {
+    const { currentTarget: input } = e;
+    this.setState(prevState => {
+      const { userData } = prevState;
+      userData[input.name] = input.value;
+      return { userData };
+    });
+  };
+
+  render() {
+    const { userData, error } = this.state;
+    return (
+      <Form autoComplete="off">
+        <InputLabel
+          label="Login"
+          error={error.login}
+          name="login"
+          value={userData.login}
+          onChange={this.handleChange}
+        />
+        <InputLabel
+          label="Name"
+          name="name"
+          error={error.name}
+          value={userData.name}
+          onChange={this.handleChange}
+        />
+        <InputLabel
+          label="Surname"
+          name="surname"
+          error={error.surname}
+          value={userData.surname}
+          onChange={this.handleChange}
+        />
+        <InputLabel
+          type="password"
+          label="Password"
+          name="password"
+          error={error.password}
+          value={userData.password}
+          onChange={this.handleChange}
+        />
+        <Button left>Sign up</Button>
+      </Form>
+    );
+  }
+}
 
 export default Registration;
