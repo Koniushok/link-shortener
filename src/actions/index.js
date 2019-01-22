ef94e7bfab36fe467c09ad16161826fa4a55921a
Valera <Koniushok@users.noreply.github.com>
2019-01-22 05:29:03 -0900

    add flow type in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,34 @@
+// @flow
 import { LINKS, FETCH_PROFILE } from "../constants/actionTypes";
 
+type Link = {
+  url: string,
+  shortLink: string,
+  passage: number,
+  tags: Array<string>,
+  description: string
+};
+type Load = { type: LINKS.LOAD };
+type Request = { type: LINKS.REQUESTED };
+type RequestSuccess = {
+  type: LINKS.REQUESTED_SUCCEEDED,
+  data: Array<Link>
+};
+type RequestError = { type: LINKS.LOAD, error: string };
 export const links = {
-  load: () => ({ type: LINKS.LOAD }),
-  request: () => ({ type: LINKS.REQUESTED }),
-  requestSuccess: data => ({
+  load: (): Load => ({ type: LINKS.LOAD }),
+  request: (): Request => ({ type: LINKS.REQUESTED }),
+  requestSuccess: (data: Array<Link>): RequestSuccess => ({
     type: LINKS.REQUESTED_SUCCEEDED,
     data
   }),
-  requestError: error => ({ type: LINKS.REQUESTED_FAILED, error })
+  requestError: (error: string): RequestError => ({
+    type: LINKS.REQUESTED_FAILED,
+    error
+  })
 };
 
-export const fetchProfile = () => ({ type: FETCH_PROFILE });
+type FetchProfile = { type: LINKS.REQUESTED };
+export const fetchProfile = (): FetchProfile => ({ type: FETCH_PROFILE });
+
+export type Actions = Link | Load | Request | RequestSuccess | FetchProfile;
