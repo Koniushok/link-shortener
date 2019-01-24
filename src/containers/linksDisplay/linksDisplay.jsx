c88d969e324d867f170afb285c7ab559a472cb0a
Valera <Koniushok@users.noreply.github.com>
2019-01-24 05:36:45 -0900

    Add implementation in linksDisplay

--- src/containers/linksDisplay/linksDisplay.jsx
+++ src/containers/linksDisplay/linksDisplay.jsx
@@ -1,14 +1,22 @@
 // @flow
 import React, { Component } from "react";
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes, css } from "styled-components";
 import { Update } from "styled-icons/material/Update";
 import { LoaderAlt } from "styled-icons/boxicons-regular/LoaderAlt";
+import { Table } from "styled-icons/icomoon/Table";
+import { List } from "styled-icons/boxicons-regular/List";
 import { connect } from "react-redux";
 import { NavLink as RouterLink } from "react-router-dom";
 import { links as linksActions } from "../../actions/index";
 import TableLink from "./tableLink";
+import TableList from "./tableList";
 import Alert from "../../components/alert";
 
+const ControlPanel = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 5px 10px;
+`;
 const rotate = keyframes`
   from { transform: rotate(0deg); }
   to { transform: rotate(360deg); }
@@ -25,6 +33,39 @@ const ResetButton = styled(Update)`
   height: 30px;
   display: inline-block;
   cursor: pointer;
+`;
+const TableTypeButton = styled(Table)`
+  width: 30px;
+  background: #e1e1e1;
+  height: 30px;
+  box-shadow: inset 0 1px 1px 1px rgba(0, 0, 0, 0.5),
+    inset 0 0 0 60px rgba(0, 0, 0, 0), 0 1px rgba(255, 255, 255, 0.08);
+  color: #000000;
+  padding: 0 5px;
+  ${props => props.enabled && StyleEnabledButton}
+  cursor: pointer;
+  :hover {
+    background: #ffd600;
+    color: black;
+  }
+`;
+const ListTypeButton = styled(List)`
+  width: 30px;
+  background: #e1e1e1;
+  height: 30px;
+  box-shadow: inset 0 1px 1px 1px rgba(0, 0, 0, 0.5),
+    inset 0 0 0 60px rgba(0, 0, 0, 0), 0 1px rgba(255, 255, 255, 0.08);
+  color: #000000;
+  ${props => props.enabled && StyleEnabledButton}
+  cursor: pointer;
+  :hover {
+    background: #ffd600;
+    color: black;
+  }
+`;
+const StyleEnabledButton = css`
+  background: #ffd600;
+  color: black;
 `;
 const NavLink = styled(RouterLink).attrs({
   activeClassName: "selected"
@@ -63,12 +104,23 @@ type Props = {
   loadMyLinks: () => void,
   typeLoad: "my" | "all"
 };
-class LinksDisplay extends Component<Props> {
+type State = {
+  typeDisplay: "table" | "list"
+};
+class LinksDisplay extends Component<Props, State> {
   constructor(props) {
     super(props);
     this.loadLinks();
   }
-
+  state = {
+    typeDisplay: "table"
+  };
+  typeDisplayTable = () => {
+    this.setState({ typeDisplay: "table" });
+  };
+  typeDisplayList = () => {
+    this.setState({ typeDisplay: "list" });
+  };
   componentDidUpdate(prevProps) {
     if (prevProps.typeLoad !== this.props.typeLoad) this.loadLinks();
   }
@@ -80,17 +132,33 @@ class LinksDisplay extends Component<Props> {
 
   render() {
     const { linksList, error, loading } = this.props;
+    const { typeDisplay } = this.state;
     return (
       <DisplayWrapper>
         {error && <Alert type="error">{error}</Alert>}
-        <NavLink to="/links/my">My links</NavLink>
-        <NavLink to="/links/all">All links</NavLink>
-        {loading ? (
-          <ResetIndicator />
-        ) : (
-          <ResetButton onClick={this.loadLinks} />
-        )}
-        <TableLink linksList={linksList} />
+        <ControlPanel>
+          <nav>
+            <NavLink to="/links/my">My links</NavLink>
+            <NavLink to="/links/all">All links</NavLink>
+          </nav>
+          <div>
+            {loading ? (
+              <ResetIndicator />
+            ) : (
+              <ResetButton onClick={this.loadLinks} />
+            )}
+            <TableTypeButton
+              onClick={this.typeDisplayTable}
+              enabled={typeDisplay === "table"}
+            />
+            <ListTypeButton
+              onClick={this.typeDisplayList}
+              enabled={typeDisplay === "list"}
+            />
+          </div>
+        </ControlPanel>
+        {typeDisplay === "table" && <TableLink linksList={linksList} />}
+        {typeDisplay === "list" && <TableList linksList={linksList} />}
       </DisplayWrapper>
     );
   }
