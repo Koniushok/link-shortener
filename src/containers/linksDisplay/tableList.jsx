f41419d8f61637ff840122836817a82d38806cee
Valera <Koniushok@users.noreply.github.com>
2019-01-24 05:34:40 -0900

    add tableList for linksDisplay

---
+++ src/containers/linksDisplay/tableList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import InfoLink from "../../components/infoLink";
+
+const LinkWrapper = styled.div`
+  margin: 10px 15px;
+  box-shadow: inset 1px 0 0 #dadce0, inset -1px 0 0 #dadce0,
+    0 1px 2px 0 rgba(60, 64, 67, 0.3), 0 1px 3px 1px rgba(60, 64, 67, 0.15);
+`;
+type Link = {
+  url: string,
+  shortLink: string,
+  passage: number,
+  tags: Array<string>,
+  description: string
+};
+const TableList = ({ linksList }: { linksList: ?Array<Link> }) => (
+  <div>
+    {linksList &&
+      linksList.map(link => (
+        <LinkWrapper key={link.shortLink}>
+          <InfoLink link={link} />
+        </LinkWrapper>
+      ))}
+  </div>
+);
+
+export default TableList;
