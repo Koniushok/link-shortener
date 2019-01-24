b45727d371a1000c27f8472e3c24ee3a015c47c1
Valera <Koniushok@users.noreply.github.com>
2019-01-24 03:24:28 -0900

    add button in alert component

--- src/components/alert.jsx
+++ src/components/alert.jsx
@@ -1,11 +1,10 @@
 // @flow
-import { type ComponentType } from "react";
+import React, { type ComponentType } from "react";
+import { Close } from "styled-icons/material/Close";
 import styled from "styled-components";
 
 type AlertType = "error" | "success";
-type Props = {
-  type: AlertType
-};
+
 function getBackground(type: AlertType): string {
   if (type === "error") return "#f8d7da";
   if (type === "success") return "#d4edda";
@@ -16,10 +15,34 @@ function getFontColor(type: AlertType): string {
   if (type === "success") return "#155724";
   return "#ccc";
 }
-const Alert: ComponentType<Props> = styled.div`
+const AlertWrapper: ComponentType<{ type: AlertType }> = styled.div`
+  position: relative;
   padding: 5px 10px;
   background: ${props => getBackground(props.type)};
   color: ${props => getFontColor(props.type)};
 `;
+const Button = styled(Close)`
+  cursor: pointer;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 20px;
+  height: 20px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+type Props = {
+  children: string,
+  type: AlertType,
+  handlerClose?: () => {}
+};
+const Alert = ({ children, type, handlerClose }: Props) => (
+  <AlertWrapper type={type}>
+    {children}
+    {handlerClose && <Button onClick={handlerClose} />}
+  </AlertWrapper>
+);
 
 export default Alert;
