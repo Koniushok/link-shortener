5ea1facea0146c3024c5dc4e2bc36525e7b6d281
Valera <Koniushok@users.noreply.github.com>
2019-01-25 03:01:39 -0900

    fix links saga

--- src/sagas/links.js
+++ src/sagas/links.js
@@ -1,25 +1,22 @@
 // @flow
-import { put, call, takeEvery, all } from "redux-saga/effects";
+import { put, call, takeLatest } from "redux-saga/effects";
 import type { Saga } from "redux-saga";
-import { links } from "../actions";
+import { links, type LinksActions } from "../actions";
 import { LINKS } from "../constants/actionTypes";
 import { getAllLinks, getMyLinks } from "../api";
 
-export function* linksLoad(type: "all" | "my"): Saga<void> {
+export function* linksLoad(action: LinksActions): Saga<void> {
   try {
     yield put(links.request());
     let data;
-    if (type === "all") data = yield call(getAllLinks);
+    if (action.type === LINKS.LOAD_ALL) data = yield call(getAllLinks);
     else data = yield call(getMyLinks);
     yield put(links.requestSuccess(data));
   } catch (error) {
-    yield put(links.requestError(error));
+    yield put(links.requestError(error.message));
   }
 }
 
 export default function* watchLinksLoad(): any {
-  yield all([
-    takeEvery(LINKS.LOAD_ALL, linksLoad, "all"),
-    takeEvery(LINKS.LOAD_MY, linksLoad, "my")
-  ]);
+  yield takeLatest([LINKS.LOAD_ALL, LINKS.LOAD_MY], linksLoad);
 }
