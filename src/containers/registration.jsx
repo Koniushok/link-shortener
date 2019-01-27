70736ad128752e938044f24d1d2246335d619d5f
Valera <Koniushok@users.noreply.github.com>
2019-01-27 09:44:03 -0900

    add form wrapper for registration containers

--- src/containers/registration.jsx
+++ src/containers/registration.jsx
@@ -1,8 +1,20 @@
 // @flow
 import React, { Component } from "react";
+import styled from "styled-components";
 import Input from "../components/input";
 import Button from "../components/button";
 import Form from "../components/form";
+
+const FormWrapper = styled.section`
+  flex: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  form {
+    max-width: 300px;
+  }
+`;
 
 type State = {
   userData: {
@@ -47,38 +59,41 @@ class Registration extends Component<any, State> {
   render() {
     const { userData, error } = this.state;
     return (
-      <Form autoComplete="off">
-        <Input
-          label="Login"
-          error={error.login}
-          name="login"
-          value={userData.login}
-          onChange={this.handleChange}
-        />
-        <Input
-          label="Name"
-          name="name"
-          error={error.name}
-          value={userData.name}
-          onChange={this.handleChange}
-        />
-        <Input
-          label="Surname"
-          name="surname"
-          error={error.surname}
-          value={userData.surname}
-          onChange={this.handleChange}
-        />
-        <Input
-          type="password"
-          label="Password"
-          name="password"
-          error={error.password}
-          value={userData.password}
-          onChange={this.handleChange}
-        />
-        <Button alignRight>Sign up</Button>
-      </Form>
+      <FormWrapper>
+        <h1>SING UP</h1>
+        <Form autoComplete="off">
+          <Input
+            label="Login"
+            error={error.login}
+            name="login"
+            value={userData.login}
+            onChange={this.handleChange}
+          />
+          <Input
+            label="Name"
+            name="name"
+            error={error.name}
+            value={userData.name}
+            onChange={this.handleChange}
+          />
+          <Input
+            label="Surname"
+            name="surname"
+            error={error.surname}
+            value={userData.surname}
+            onChange={this.handleChange}
+          />
+          <Input
+            type="password"
+            label="Password"
+            name="password"
+            error={error.password}
+            value={userData.password}
+            onChange={this.handleChange}
+          />
+          <Button alignRight>Sign up</Button>
+        </Form>
+      </FormWrapper>
     );
   }
 }
