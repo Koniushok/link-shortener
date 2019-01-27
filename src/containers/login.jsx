80001266c634f86e092552505399a81783491d82
Valera <Koniushok@users.noreply.github.com>
2019-01-27 01:02:40 -0900

    add LoginWrapper

--- src/containers/login.jsx
+++ src/containers/login.jsx
@@ -1,8 +1,20 @@
 // @flow
 import React, { Component } from "react";
+import styled from "styled-components";
 import Input from "../components/input";
 import Button from "../components/button";
 import Form from "../components/form";
+
+const LoginWrapper = styled.section`
+  flex: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  form {
+    max-width: 300px;
+  }
+`;
 
 type State = {
   loginData: {
@@ -38,24 +50,27 @@ class Authorization extends Component<any, State> {
   render() {
     const { loginData, error } = this.state;
     return (
-      <Form autoComplete="off">
-        <Input
-          label="Login"
-          error={error.loginName}
-          name="loginName"
-          value={loginData.loginName}
-          onChange={this.handleChange}
-        />
-        <Input
-          type="password"
-          label="Password"
-          name="password"
-          error={error.password}
-          value={loginData.password}
-          onChange={this.handleChange}
-        />
-        <Button alignRight>Log in</Button>
-      </Form>
+      <LoginWrapper>
+        <h1>SIGN IN</h1>
+        <Form autoComplete="off">
+          <Input
+            label="Login"
+            error={error.loginName}
+            name="loginName"
+            value={loginData.loginName}
+            onChange={this.handleChange}
+          />
+          <Input
+            type="password"
+            label="Password"
+            name="password"
+            error={error.password}
+            value={loginData.password}
+            onChange={this.handleChange}
+          />
+          <Button alignRight>Log in</Button>
+        </Form>
+      </LoginWrapper>
     );
   }
 }
