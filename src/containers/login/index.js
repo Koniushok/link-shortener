cd13fcd824506a97e4098162dc485505ad797f7f
Valera <Koniushok@users.noreply.github.com>
2019-01-27 06:49:04 -0900

    connect user state to login

--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,2 +1,20 @@
 // @flow
-export { default } from "./login";
+import { connect } from "react-redux";
+import { login } from "../../actions/user";
+import Login from "./login";
+
+const mapStateToProps = ({ user }) => ({
+  error: user.error,
+  loading: user.loading,
+  auth: user.auth
+});
+
+const mapDispatchToProps = dispatch => ({
+  loginRequest: (password: string, loginName: string) =>
+    dispatch(login(password, loginName))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Login);
