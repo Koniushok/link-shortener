056fc176fad7ff48618fabf99300bad2442ece01
Valera <Koniushok@users.noreply.github.com>
2019-01-28 10:25:08 -0900

    add linkCreator saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -3,7 +3,8 @@ import { all } from "redux-saga/effects";
 import type { Saga } from "redux-saga";
 
 import linksSaga from "./links";
+import linkCreatorSaga from "./linkCreator";
 
 export default function* rootSaga(): Saga<void> {
-  yield all([linksSaga()]);
+  yield all([linksSaga(), linkCreatorSaga()]);
 }
---
+++ src/sagas/linkCreator.js
@@ -0,0 +1,25 @@
+// @flow
+import { put, call, takeLatest } from "redux-saga/effects";
+import type { Saga } from "redux-saga";
+import {
+  createLinkFailed,
+  createLinkRequested,
+  createLinkSucceeded,
+  type CreateLink
+} from "../actions/linkCreator";
+import { CREATE_LINK } from "../constants/actionTypes";
+import { createLink } from "../api";
+
+export function* create(action: CreateLink): Saga<void> {
+  try {
+    yield put(createLinkRequested());
+    const result = yield call(createLink, action.payload);
+    yield put(createLinkSucceeded(result));
+  } catch (error) {
+    yield put(createLinkFailed(error.message));
+  }
+}
+
+export default function* watchCreateLink(): any {
+  yield takeLatest(CREATE_LINK, create);
+}
