5d62d06781ea5d66e7bf6b7000281d2c01f8b816
Valera <Koniushok@users.noreply.github.com>
2019-01-28 10:24:32 -0900

    add linkCreator reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,17 @@
 import { combineReducers, type CombinedReducer } from "redux";
 import type { Actions } from "../actions";
 import linksReducers, { type State as LinksState } from "./links";
+import linkCreatorReducer, {
+  type State as linkCreatorState
+} from "./linkCreator";
 
 export type State = {
-  links: LinksState
+  links: LinksState,
+  linkCreator: linkCreatorState
 };
 const reducers = {
-  links: linksReducers
+  links: linksReducers,
+  linkCreator: linkCreatorReducer
 };
 const rootReducers: CombinedReducer<State, Actions> = combineReducers(reducers);
 
---
+++ src/reducers/linkCreator.js
@@ -0,0 +1,36 @@
+// @flow
+import {
+  CREATE_LINK_REQUESTED,
+  CREATE_LINK_FAILED,
+  CREATE_LINK_SUCCEEDED
+} from "../constants/actionTypes";
+import { type LinkCreatorActions } from "../actions/linkCreator";
+
+export type State = {
+  +result: string,
+  +error: string,
+  +loading: boolean
+};
+const initialState: State = { result: "", error: "", loading: false };
+
+const linkCreatorReducer = (
+  state: State = initialState,
+  action: LinkCreatorActions
+): State => {
+  switch (action.type) {
+    case CREATE_LINK_REQUESTED:
+      return { ...state, result: "", error: "", loading: true };
+    case CREATE_LINK_FAILED:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false
+      };
+    case CREATE_LINK_SUCCEEDED:
+      return { ...state, result: action.payload, loading: false };
+    default:
+      return state;
+  }
+};
+
+export default linkCreatorReducer;
