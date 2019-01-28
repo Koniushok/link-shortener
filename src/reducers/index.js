2cdf611c66865074e86788fe815aee15c4e48a38
Valera <Koniushok@users.noreply.github.com>
2019-01-28 02:42:03 -0900

    add auth redurers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,14 +3,17 @@ import { combineReducers, type CombinedReducer } from "redux";
 import type { Actions } from "../actions";
 import linksReducers, { type State as LinksState } from "./links";
 import userReducers, { type State as UserState } from "./user";
+import authReducers, { type State as AuthState } from "./auth";
 
 export type State = {
   links: LinksState,
-  user: UserState
+  user: UserState,
+  auth: AuthState
 };
 const reducers = {
   links: linksReducers,
-  user: userReducers
+  user: userReducers,
+  auth: authReducers
 };
 const rootReducers: CombinedReducer<State, Actions> = combineReducers(reducers);
 
