f679d17c8bb7a144e8e43d2768953e70706c6ad5
Valera <Koniushok@users.noreply.github.com>
2019-01-29 04:15:23 -0900

    add linkDisplay container

---
+++ src/containers/linkDisplay/index.js
@@ -0,0 +1,17 @@
+// @flow
+import { connect } from "react-redux";
+import { fetchLink } from "../../actions/link";
+import LinkDisplay from "./linkDisplay";
+
+const mapStateToProps = ({ link }) => ({
+  link: link.data,
+  error: link.error,
+  loading: link.loading
+});
+
+const mapDispatchToProps = { fetchLink };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LinkDisplay);
---
+++ src/containers/linkDisplay/linkDisplay.jsx
@@ -0,0 +1,31 @@
+// @flow
+import React, { Component, Fragment } from "react";
+import { type Link } from "../../types";
+import LinkInf from "../../components/infoLink";
+import Alert from "../../components/alert";
+
+type Props = {
+  linkId: string,
+  link: Link,
+  error: string,
+  fetchLink: (id: string) => void
+};
+
+class LinkDisplay extends Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.props.fetchLink(this.props.linkId);
+  }
+
+  render() {
+    const { error, link } = this.props;
+    return (
+      <Fragment>
+        {error && <Alert type="error">{error}</Alert>}
+        {link && <LinkInf link={link} />}
+      </Fragment>
+    );
+  }
+}
+
+export default LinkDisplay;
