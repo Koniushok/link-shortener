449035fb00e502b272077e91840a15d739208375
Valera <Koniushok@users.noreply.github.com>
2019-02-06 21:35:45 -0900

    add redux compose

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
 // @flow
-import { createStore, applyMiddleware, type Store } from 'redux';
+/* eslint-disable no-underscore-dangle */
+import {
+  createStore, applyMiddleware, compose, type Store,
+} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer, { type State } from '../reducers';
 import rootSaga from '../sagas';
 import { type Actions } from '../actions';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const sagaMiddleware: any = createSagaMiddleware();
-const store: Store<State, Actions> = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store: Store<State, Actions> = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleware)),
+);
 sagaMiddleware.run(rootSaga);
 
 export default store;
