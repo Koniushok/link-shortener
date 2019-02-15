2cd83bde1f4824b1516de5c12c22faa280167057
Valera <Koniushok@users.noreply.github.com>
2019-02-15 12:54:39 -0900

    fix tableList padding

--- src/containers/linksDisplay/tableList.jsx
+++ src/containers/linksDisplay/tableList.jsx
@@ -7,7 +7,7 @@ import { typeLinksLoad, type TypeLinksLoad } from '../../constants/display';
 
 const LinkWrapper = styled.div`
   margin: 10px 0;
-  padding: 0 5px 0 15px;
+  padding: 0 5px 1px 15px;
   border-radius: 5px;
   box-shadow: inset 1px 0 0 #dadce0, inset -1px 0 0 #dadce0, 0 1px 2px 0 rgba(60, 64, 67, 0.3),
     0 1px 3px 1px rgba(60, 64, 67, 0.15);
